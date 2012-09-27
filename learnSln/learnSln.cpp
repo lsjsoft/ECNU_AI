e50175f8aafebefa5a5fe5f70a56eedbcfe3edbd
shaojingliu <lsjsoft>
2012-09-27 19:47:56 +0800

    add force

--- learnSln/learnSln.cpp
+++ learnSln/learnSln.cpp
@@ -16,6 +16,7 @@ struct tpos
 typedef vector<tpos> tposV1;
 typedef vector<tposV1> tposV2;
 
+
 template<typename QTYPE, unsigned int _QSIZE>
 struct TQueen 
 {
@@ -28,153 +29,147 @@ struct TQueen
 		memset(this, 0, sizeof(*this));
 	}
 
-	void get_infaline(unsigned int x, unsigned int y, tposV2& r)
+	void get_infaline(unsigned int x, unsigned int y, tposV2& out) const
 	{
-		tposV1 v;
+		tposV1 line;
 
 		for(int ax=0; ax<QSIZE; ++ax)
 		{
 			if (ax!= x)
 			{
-				v.push_back(tpos(ax, y));
+				line.push_back(tpos(ax, y));
 			}
 		}
-		r.push_back(v);
-		v.clear();
+		out.push_back(line);
+		line.clear();
 
 		for(int ay=0; ay<QSIZE; ++ay)
 		{
 			if (ay!= y)
 			{
-				v.push_back(tpos(x, ay));
+				line.push_back(tpos(x, ay));
 			}
 		}
-		r.push_back(v);
-		v.clear();
+		out.push_back(line);
+		line.clear();
 
 		for(int ax=x+1, ay=y+1; ax<QSIZE && ax<QSIZE; ++ax, ++ay)
 		{
-			v.push_back( tpos(ax, ay));
+			line.push_back( tpos(ax, ay));
 		}
-		if (!v.empty())
+		if (!line.empty())
 		{
-			r.push_back(v);
-			v.clear();
+			out.push_back(line);
+			line.clear();
 		}
 
 		for(int ax=x+1, ay=y-1; ax<QSIZE && ay>=0; ++ax, --ay)
 		{
-			v.push_back( tpos(ax, ay));
+			line.push_back( tpos(ax, ay));
 		}
-		if (!v.empty())
+		if (!line.empty())
 		{
-			r.push_back(v);
-			v.clear();
+			out.push_back(line);
+			line.clear();
 		}
 
 		for(int ax=x-1, ay=y+1; ax>=0 && ax<QSIZE; --ax, ++ay)
 		{
-			v.push_back( tpos(ax, ay));
+			line.push_back( tpos(ax, ay));
 		}
-		if (!v.empty())
+		if (!line.empty())
 		{
-			r.push_back(v);
-			v.clear();
+			out.push_back(line);
+			line.clear();
 		}
 
 		for(int ax=x-1, ay=y-1; ax>=0 && ay>=0; --ax, --ay)
 		{
-			v.push_back( tpos(ax, ay));
+			line.push_back( tpos(ax, ay));
 		}
-		if (!v.empty())
+		if (!line.empty())
 		{
-			r.push_back(v);
-			v.clear();
+			out.push_back(line);
+			line.clear();
 		}
 	}
 
-	bool can_put(unsigned int r, unsigned int c, const QTYPE& v)
+	bool can_put(unsigned int y, unsigned int x, const QTYPE& v)
 	{
 		TQueen<QTYPE, QSIZE> temp = *this;
 		temp.set(r, c, v);
-
-
 	}
 
-	bool is_emtpy(const QTYPE& v)
+	bool is_emtpy(const QTYPE& v) const
 	{
 		return v == 0;
 	}
 
-	bool test_invalid() const
+	/** calc force */
+	unsigned int counter_force(const tposV2& v2) const
 	{
-		for( unsigned int y = 0; y< QSIZE; ++y)
+		unsigned int f= 0;
+		for each( const tposV1& v1 in v2)
 		{
-			unsigned int counter = 0;
-			for( unsigned int x = 0; x < QSIZE; ++x)
+			for each( const tpos& v in v1)
 			{
-				const QTYPE& v= get(y, x);
-				if (!is_emtpy(v))
+				if (is_emtpy( get(v.y, v.x)))
 				{
-					++counter;
+					++f;
 				}
 			}
-
-			if ( counter > 1)
-			{
-				return false;
-			}
 		}
+		return f;
+	}
 
-		for( unsigned int x = 0; x< QSIZE; ++x)
+	bool is_valid() const
+	{
+		for(unsigned int y= 0; y< QSIZE; ++y)
 		{
-			unsigned int counter = 0;
-			for( unsigned int y = 0; y < QSIZE; ++y)
+			for(unsigned int x=0; x< QSIZE; ++x)
 			{
-				const QTYPE& v= get(y, x);
-				if (!is_emtpy(v))
+				if (!is_emtpy(get(y, x)))
 				{
-					++counter;
+					tposV2 pv;
+					get_infaline(x, y, pv);
+					if (counter_force(pv) > 0)
+					{
+						return false;
+					}
 				}
 			}
-
-			if ( counter > 1)
-			{
-				return false;
-			}
 		}
-
-
-		for()
+		
+		return true;
 	}
 
-	const QTYPE& get(unsigned int r, unsigned int c)
+	const QTYPE& get(unsigned int y, unsigned int x) const
 	{
-		return bound[r][c];
+		return bound[y][x];
 	}
 
-	void set(const QTYPE& v, unsigned int r, unsigned int c)
+	void set(unsigned int y, unsigned int x, const QTYPE& v)
 	{
-		bound[r][c]= v;
+		bound[y][x]= v;
 	}
 };
 
-TQueen<int, 8> obj;
 
 int main(int argc, _TCHAR* argv[])
 {
-	tposV2 v;
-	obj.get_infaline(0, 0, v);
+	TQueen<int, 8> obj;
 
-	for each( const tposV1& p in v)
-	{
-		for each( const tpos& k in p)
-		{
-			printf("[%d,%d] ", k.x, k.y);
-		}
+	obj.set(0, 0, 1);
+	const char* text1= obj.is_valid() ? "valid" : "invalid";
 
-		printf("\n");
-	}
+	obj.set(1, 1, 1);
+	const char* text2= obj.is_valid() ? "valid" : "invalid";
+
+	printf(text1);
+	printf("\n");
+	
+	printf(text2);
+	printf("\n");
 
 	return 0;
 }
