7a2f50d3dddc917434547838bd385cdb9f9dfd8c
shaojingliu <shaojingliu@163.com>
2012-09-29 11:44:18 +0800

    add

--- learnSln/learnSln.cpp
+++ learnSln/learnSln.cpp
@@ -1,6 +1,7 @@
 
 #include "stdafx.h"
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -15,131 +16,266 @@ struct tpos
 
 typedef vector<tpos> tposV1;
 typedef vector<tposV1> tposV2;
+typedef std::stack<tpos> tposStk;
 
+class Tbase
+{
+public:
+	static const char* tostr(int i)
+	{
+		static char buff[32];
+		sprintf_s(buff, "%d", i);
+		return buff;
+	}
+
+	static const char* tostr(unsigned int i)
+	{
+		static char buff[32];
+		sprintf_s(buff, "%d", i);
+		return buff;
+	}
+};
 
 template<typename QTYPE, unsigned int _QSIZE>
 struct TQueen 
 {
-	enum { QSIZE=_QSIZE };
+	enum { QSIZE=_QSIZE, FORCE_DIR= 4, };
 
 	QTYPE bound[QSIZE][QSIZE];
+
+	struct flag_line
+	{
+		flag_line(): size(0) {}
+		tpos pos[QSIZE];
+		unsigned int size;
+
+		void add(unsigned int x, unsigned int y)
+		{
+			pos[size].x = x;
+			pos[size].y = y;
+			++size;
+		}
+	};
+
+	struct flag_lines
+	{
+		flag_line lines[FORCE_DIR];
+
+		void clear()
+		{
+			for(unsigned int i=0; i<FORCE_DIR; ++i)
+			{
+				lines[i].size = 0;
+			}
+		}
+	};
+
+	struct queen_stack
+	{
+		queen_stack() : _size(0) {}
+
+		tpos pos[QSIZE];
+		unsigned int _size;
+
+		void push(unsigned int ax, unsigned int ay)
+		{
+			pos[_size].x = ax;
+			pos[_size].y = ay;
+			++_size;
+		}
+
+		void pop()
+		{
+			if (_size> 0)
+			{
+				--_size;
+			}
+		}
+
+		unsigned int size() const 
+		{
+			return _size;
+		}
+	};
 
 	TQueen()
 	{
 		memset(this, 0, sizeof(*this));
 	}
 
-	void get_infaline(unsigned int x, unsigned int y, tposV2& out) const
+	bool layout_queen(queen_stack& tk)
 	{
-		tposV1 line;
+		size_t size = tk.size();
+		for(unsigned int y=0; y<QSIZE; ++y)
+		{
+			for(unsigned int x=0; x<QSIZE; ++x)
+			{
+				if (! put_test(y, x))
+				{
+					continue;
+				}
+
+				tk.push(x, y);
+				set(y, x, create_entity());
+
+				if ( tk.size() == QSIZE)
+				{
+					return true;
+				}
+
+				if (!layout_queen(tk))
+				{
+					set(y, x, create_empty());
+					tk.pop();
+				}
+			}
+		}
+
+		return tk.size() > size;
+	}
+
+	queen_stack layout_queenx()
+	{
+		queen_stack tk;
+		layout_queen(tk);
+		return tk;
+	}
+
+	void get_influence(unsigned int x, unsigned int y, flag_lines& out) const
+	{
+		unsigned int idx= 0;
+
+		out.clear();
 
 		for(int ax=0; ax<QSIZE; ++ax)
 		{
-			if (ax!= x)
+			if (ax != x)
 			{
-				line.push_back(tpos(ax, y));
+				out.lines[idx].add(ax, y);
 			}
 		}
-		out.push_back(line);
-		line.clear();
 
+		++idx;
 		for(int ay=0; ay<QSIZE; ++ay)
 		{
 			if (ay!= y)
 			{
-				line.push_back(tpos(x, ay));
+				out.lines[idx].add(x, ay);
 			}
 		}
-		out.push_back(line);
-		line.clear();
 
-		for(int ax=x+1, ay=y+1; ax<QSIZE && ax<QSIZE; ++ax, ++ay)
+		++idx;
+		for(int ax=x+1, ay=y+1; ax<QSIZE && ay<QSIZE; ++ax, ++ay)
 		{
-			line.push_back( tpos(ax, ay));
+			out.lines[idx].add(ax, ay);
 		}
-		if (!line.empty())
-		{
-			out.push_back(line);
-			line.clear();
-		}
-
 		for(int ax=x+1, ay=y-1; ax<QSIZE && ay>=0; ++ax, --ay)
 		{
-			line.push_back( tpos(ax, ay));
-		}
-		if (!line.empty())
-		{
-			out.push_back(line);
-			line.clear();
+			out.lines[idx].add(ax, ay);
 		}
 
-		for(int ax=x-1, ay=y+1; ax>=0 && ax<QSIZE; --ax, ++ay)
+		++idx;
+		for(int ax=x-1, ay=y+1; ax>=0 && ay<QSIZE; --ax, ++ay)
 		{
-			line.push_back( tpos(ax, ay));
+			out.lines[idx].add(ax, ay);
 		}
-		if (!line.empty())
-		{
-			out.push_back(line);
-			line.clear();
-		}
-
 		for(int ax=x-1, ay=y-1; ax>=0 && ay>=0; --ax, --ay)
 		{
-			line.push_back( tpos(ax, ay));
+			out.lines[idx].add(ax, ay);
 		}
-		if (!line.empty())
+	}
+
+	bool put_test(unsigned int y, unsigned int x, bool log_invalid=false) const
+	{
+		if (!is_valid())
 		{
-			out.push_back(line);
-			line.clear();
+			return false;
 		}
+
+		if (!is_empty_pos(y, x))
+		{
+			return false;
+		}
+
+		TQueen<QTYPE, QSIZE> obj(*this);
+		obj.set(y, x, create_entity() );
+		bool result= obj.is_valid();
+
+		if (log_invalid && !result)
+		{
+			printf("invalid!\n");
+			obj.view();
+		}
+
+		return result;
 	}
 
-	bool can_put(unsigned int y, unsigned int x, const QTYPE& v)
+	bool is_emtpy_element(const QTYPE& v) const
 	{
-		TQueen<QTYPE, QSIZE> temp = *this;
-		temp.set(r, c, v);
+		return v == QTYPE(0);
 	}
 
-	bool is_emtpy(const QTYPE& v) const
+	QTYPE create_entity() const
 	{
-		return v == 0;
+		return QTYPE(1);
 	}
 
-	/** calc force */
-	unsigned int counter_force(const tposV2& v2) const
+	QTYPE create_empty() const 
+	{
+		return QTYPE(0);
+	}
+
+	void print_element(const QTYPE& v)
+	{
+		printf("%d", v);
+	}
+
+	void _sprint_element(std::string& str, const QTYPE& v)
+	{
+		str += Tbase::tostr(v);
+	}
+
+	bool is_empty_pos(unsigned int y, unsigned int x) const
+	{
+		return is_emtpy_element( get(y, x) );
+	}
+
+	unsigned int counter_force(const flag_lines& lineobj) const
 	{
 		unsigned int f= 0;
-		for each( const tposV1& v1 in v2)
+		for( unsigned int i=0; i<FORCE_DIR; ++i)
 		{
-			for each( const tpos& v in v1)
+			const flag_line& curr= lineobj.lines[i];
+
+			for(unsigned int j=0; j<curr.size; ++j)
 			{
-				if (is_emtpy( get(v.y, v.x)))
+				if (!is_empty_pos(curr.pos[j].y, curr.pos[j].x))
 				{
 					++f;
 				}
 			}
 		}
+
 		return f;
 	}
 
 	bool is_valid() const
 	{
+		flag_lines out;
 		for(unsigned int y= 0; y< QSIZE; ++y)
 		{
 			for(unsigned int x=0; x< QSIZE; ++x)
 			{
-				if (!is_emtpy(get(y, x)))
+				if (!is_empty_pos(y, x))
 				{
-					tposV2 pv;
-					get_infaline(x, y, pv);
-					if (counter_force(pv) > 0)
+					get_influence(x, y, out);
+					if (counter_force(out) > 0)
 					{
 						return false;
 					}
 				}
 			}
 		}
-		
 		return true;
 	}
 
@@ -152,24 +288,72 @@ struct TQueen
 	{
 		bound[y][x]= v;
 	}
+
+	void _sprint_head(std::string& str)
+	{
+		str += "   ";
+
+		for(unsigned int i=0; i<QSIZE; ++i)
+		{
+			str+= Tbase::tostr(i);
+			str+= " ";
+		}
+
+		str += "\n ©°";
+
+		for(unsigned int i=0; i<QSIZE; ++i)
+		{
+			str+= "©¤";
+		}
+
+		str += "©´\n";
+	}
+
+	void _sprint_cont(std::string& str)
+	{
+ 		for(unsigned int y=0; y<QSIZE; ++y)
+ 		{
+			str += Tbase::tostr(y);
+			str += "©¦";
+
+ 			for(unsigned int x=0; x<QSIZE; ++x)
+ 			{
+ 				const QTYPE& element= get(y, x);
+				_sprint_element(str, element);
+				str += " ";
+ 			}
+
+ 			str += "©¦\n";
+ 		}
+	}
+
+	void _sprint_end(std::string& str)
+	{
+		str += " ©¸";
+
+		for(unsigned int i=0; i<QSIZE; ++i)
+		{
+			str+= "©¤";
+		}
+
+		str += "©¼\n";
+	}
+
+	void view()
+	{
+		std::string str;
+		_sprint_head(str);
+		_sprint_cont(str);
+		_sprint_end(str);
+		printf(str.c_str());
+	}
 };
 
 
 int main(int argc, _TCHAR* argv[])
 {
-	TQueen<int, 8> obj;
-
-	obj.set(0, 0, 1);
-	const char* text1= obj.is_valid() ? "valid" : "invalid";
-
-	obj.set(1, 1, 1);
-	const char* text2= obj.is_valid() ? "valid" : "invalid";
-
-	printf(text1);
-	printf("\n");
-	
-	printf(text2);
-	printf("\n");
-
+	typedef TQueen<int, 8> TQueen8;
+	TQueen8 obj;
+	TQueen8::queen_stack k= obj.layout_queenx();
 	return 0;
 }
